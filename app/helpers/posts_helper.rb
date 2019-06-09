3d85c6decd1ae9beb0048fcc9ece77d1e7b4419b
seosd97 <seosd97@gmail.com>
2019-06-09 18:38:12 +0900

    change post element hover style and add description

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,16 @@
 module PostsHelper
-    def md_to_html(text)
+    require 'redcarpet'
+    require 'redcarpet/render_strip'
+
+    def parse_md_to_html(text)
         renderer = Redcarpet::Render::HTML::new({ hard_wrap: true })
         markdown = Redcarpet::Markdown::new(renderer, {})
         markdown.render(text).html_safe;
+    end
+
+    def parse_md_strip_down(text)
+        markdown = Redcarpet::Markdown::new(Redcarpet::Render::StripDown)
+        markdown.render(text)
     end
 
     def get_post_path(post)
