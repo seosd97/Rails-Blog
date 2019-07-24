294ab762f49ff68d44eddd1a35e9ba99d8183356
seosd97 <seosd97@gmail.com>
2019-07-24 20:23:51 +0900

    show warning alert when quit post edit page

--- app/assets/javascripts/post_edit.js
+++ app/assets/javascripts/post_edit.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+window.addEventListener("load", () => {
     const descEditField = document.querySelector('.post-edit-field-desc');
     const descHiddenField = document.querySelector('.post-edit-desc-hidden');
 
@@ -17,4 +17,24 @@ window.onload = () => {
             Rails.fire(form, 'submit');
         };
     }
-}
+}, false);
+
+// NOTE : safari에서 BFCache를 사용하기 때문에 최초 1회 후에는 페이지에서 나갈 때 체크해주지 않음.
+//        그래서 onpageshow에서 persisted(BFCache를 사용하여 로드했는지 체크하는 변수)를 사용하여 체크해줘야함.
+//        하지만 chrome과 safari에서 persisted에 버그가 있기 때문에 정확한 동작을 하지 않음.
+//        window.performance.navigation.type이 2이면 BFCache를 사용한 것이라고 하는데 동작하지 않음.
+
+// window.addEventListener("pageshow", (e) => {
+//     if (e.persisted || (window.performance && window.performance.navigation.type == 2)) {
+//         console.log("cached page");
+//         window.location.reload();
+//     }
+// }, false);
+
+window.addEventListener("beforeunload", (e) => {
+    if (e) {
+        e.returnValue = "close?";
+    }
+
+    return "close?";
+}, false);
