bd88690c578fddf1061303275b0c3e072f60397a
seosd <seosd97@gmail.com>
2020-03-10 16:03:11 +0900

    remove code related to carrierwave

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Post < ApplicationRecordPostImagesUploader
+class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     validates :title, presence: true, length: { minimum: 2 }
     validates :description, presence: true, length: { minimum: 2 }
