3b1238e31c828e31d34269807083c7d314eb0d66
seosd <seosd97@gmail.com>
2020-03-12 12:09:14 +0900

    add defer option when load js

--- app/assets/javascripts/post_edit.js
+++ app/assets/javascripts/post_edit.js
@@ -1,55 +1,60 @@
-window.addEventListener(
-    "load",
-    () => {
-        const descEditField = document.querySelector(".post-edit-field-desc");
-        const descHiddenField = document.querySelector(".post-edit-desc-hidden");
+const descEditField = document.querySelector(".post-edit-field-desc");
+const descHiddenField = document.querySelector(".post-edit-desc-hidden");
+const imageField = document.querySelector(".image-field");
+const imageHidden = document.querySelector(".hidden-file-field");
 
-        if (descHiddenField.value !== "") {
-            descEditField.innerText = descHiddenField.value;
-        }
+if (descHiddenField.value !== "") {
+    descEditField.innerText = descHiddenField.value;
+}
 
-        const publishBtn = document.querySelector(".publish-button");
-        if (publishBtn !== null) {
-            publishBtn.onclick = () => {
-                descHiddenField.value = descEditField.innerText;
-                const form = document.querySelector(".post-edit-form");
-                Rails.fire(form, "submit");
-            };
-        }
+const publishBtn = document.querySelector(".publish-button");
+if (publishBtn !== null) {
+    publishBtn.onclick = () => {
+        descHiddenField.value = descEditField.innerText;
+        const form = document.querySelector(".post-edit-form");
+        Rails.fire(form, "submit");
+    };
+}
 
-        const uploadImageBtn = document.querySelector(".upload-image-button");
-        if (uploadImageBtn !== null) {
-            uploadImageBtn.onclick = () => {
-                showPreviewBar();
-            };
-        }
+const uploadImageBtn = document.querySelector(".upload-image-button");
+if (uploadImageBtn !== null) {
+    uploadImageBtn.onclick = () => {
+        showPreviewBar();
+    };
+}
 
-        const uploadImage = document.querySelector(".upload-image");
-        const hiddenFileField = document.querySelector(".hidden-file-field");
-        uploadImage.onclick = () => {
-            hiddenFileField.click();
-        };
+const uploadImage = document.querySelector(".upload-image");
+const hiddenFileField = document.querySelector(".hidden-file-field");
+uploadImage.onclick = () => {
+    hiddenFileField.click();
+};
 
-        // NOTE : 이미지 프리뷰 창 이벤트
+hiddenFileField.onchange = () => {
+    const reader = new FileReader();
+    reader.readAsDataURL(hiddenFileField.files[0]);
+    console.log(hiddenFileField.files[0]);
+    // reader.onload = () => {
+    //     console.log(reader.result);
+    // }
+}
 
-        // TODO : 추후 연출을 위해 애니메이션으로 처리를 해줘야 함.
-        const imagePreviewBar = document.querySelector(".image-preview-bar");
+// NOTE : 이미지 프리뷰 창 이벤트
 
-        function showPreviewBar() {
-            imagePreviewBar.style.display = "block";
-        }
+// TODO : 추후 연출을 위해 애니메이션으로 처리를 해줘야 함.
+const imagePreviewBar = document.querySelector(".image-preview-bar");
 
-        function hidePreviewBar() {
-            imagePreviewBar.style.display = "none";
-        }
+function showPreviewBar() {
+    imagePreviewBar.style.display = "block";
+}
 
-        const previewCloseButton = document.querySelector(".preview-close");
-        previewCloseButton.onclick = () => {
-            hidePreviewBar();
-        };
-    },
-    false
-);
+function hidePreviewBar() {
+    imagePreviewBar.style.display = "none";
+}
+
+const previewCloseButton = document.querySelector(".preview-close");
+previewCloseButton.onclick = () => {
+    hidePreviewBar();
+};
 
 // NOTE : safari에서 BFCache를 사용하기 때문에 최초 1회 후에는 페이지에서 나갈 때 체크해주지 않음.
 //        그래서 onpageshow에서 persisted(BFCache를 사용하여 로드했는지 체크하는 변수)를 사용하여 체크해줘야함.
--- app/assets/javascripts/post_edit_navigation.js
+++ app/assets/javascripts/post_edit_navigation.js
@@ -1,21 +1,19 @@
-window.addEventListener("load", () => {
-    const addImageBtn = document.querySelector('.nav-add-image');
-    const fileField = document.querySelector('.nav-hidden-file-field');
+const addImageBtn = document.querySelector('.nav-add-image');
+const fileField = document.querySelector('.nav-hidden-file-field');
 
-    if (addImageBtn === null || fileField === null)
+if (addImageBtn === null || fileField === null)
+    return;
+
+addImageBtn.onclick = () => {
+    fileField.click();
+};
+
+fileField.onchange = () => {
+    const editArea = document.querySelector('.post-edit-field-desc');
+    let files = fileField.files;
+
+    if (files.length <= 0)
         return;
 
-    addImageBtn.onclick = () => {
-        fileField.click();
-    };
-
-    fileField.onchange = () => {
-        const editArea = document.querySelector('.post-edit-field-desc');
-        let files = fileField.files;
-
-        if (files.length <= 0)
-            return;
-
-        
-    }
-}, false);
+    
+}
