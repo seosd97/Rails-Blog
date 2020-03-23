f75c4b4d06b2ad3838e6c6b9e144e67f11a771d9
seosd <seosd97@gmail.com>
2020-03-23 18:54:10 +0900

    create user authentication form

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,9 +2,41 @@ class AuthController < ApplicationController
     def signup
     end
 
+    def regist
+        if User.where("name = \"#{params[:name]}\" OR email = \"#{params[:email]}\"").present?
+            redirect_to "/" and return
+        end
+
+        user = User.new(name: params[:name], email: params[:email], password: params[:password], role: "admin");
+        if user.save
+            session[:user_id] = user.id;
+            redirect_to posts_path
+        end
+    end
+
     def signin
     end
 
+    def create_session
+        user = User.where("name = \"#{params[:identity]}\" OR email = \"#{params[:identity]}\"").first;
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to posts_path
+        else
+            redirect_to join_path
+        end
+    end
+
     def signout
+        session.delete(:user_id);
+        redirect_to posts_path
+    end
+
+    def destroy_session
+    end
+
+    private
+    def permit_params(key, *values)
+        params.require(key).permit(values);
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'login', to: 'auth#signin'
+  post 'login', to: 'auth#create_session'
   get 'join', to: 'auth#signup'
+  post 'join', to: 'auth#regist'
   get 'logout', to: 'auth#signout'
 
   resources :posts do
